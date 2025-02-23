6fdf4abf837f30bd6e408bbe9be18e8660598349
clewisit <51469926+clewisit@users.noreply.github.com>
2025-02-23 04:26:34 -0800

    Add FOSS target for MicroG
    added 64FN (gsi_arm64_fN) build target for MicroG build.

--- build_unified.sh
+++ build_unified.sh
@@ -107,6 +107,7 @@ build_treble() {
     case "${1}" in
         ("64VN") TARGET=gsi_arm64_vN;;
         ("64GN") TARGET=gsi_arm64_gN;;
+        ("64FN") TARGET=gsi_arm64_fN;;
         (*) echo "Invalid target - exiting"; exit 1;;
     esac
     lunch lineage_${TARGET}-${aosp_target_release}-userdebug
