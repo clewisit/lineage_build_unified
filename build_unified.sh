96c7a61f801f85455f9d78472fe730ca965ebea3
clewisit <51469926+clewisit@users.noreply.github.com>
2025-07-06 10:27:40 -0700

    Update build_unified.sh
    set repo sync to use 8 threads to prevent server throttling.  sign all builds.

--- build_unified.sh
+++ build_unified.sh
@@ -61,7 +61,7 @@ prep_build() {
     echo ""
 
     echo "Syncing repos"
-    repo sync -c --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+    repo sync -c --force-sync --no-clone-bundle --no-tags -j8
     echo ""
 
     echo "Setting up build environment"
@@ -111,7 +111,7 @@ build_treble() {
     make installclean
     WITH_ADB_INSECURE=true make -j$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l) systemimage
     SIGNED=false
-    if [ ${SIGNABLE} = true ] && [[ ${TARGET} == *_g? ]]
+    if [ ${SIGNABLE} = true ]
     then
         WITH_ADB_INSECURE=true make -j$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l) target-files-package otatools
         bash ./lineage_build_unified/sign_target_files.sh $OUT/signed-target_files.zip
